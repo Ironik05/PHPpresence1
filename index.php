ddf6c3a6bebc6368c4ca876d277ccdc2e60913f3
Ironik05 <florence.d.penicaud@gmail.com>
2025-10-29 11:39:53 +0100

    fin_de_journee

--- index.php
+++ index.php
@@ -8,8 +8,27 @@ declare (strict_types=1);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body>g
     <?php
+    //Jour 1
+    //Exercice 1
+    $nom = "Call of Duty";
+    $prix = 60;
+    $plateforme = "PSS";
+     //Definition d'une classe 
+    class JeuVideo {
+     //Proprietes
+        public $nom;
+        public $prix;
+        public $plateforme;
+
+     //Constructeur
+     public function _construct($nom, $prix, $plateforme = null) {
+        $this->nom = $nom;
+        $this->prix = $prix;
+        $this->plateforme = $plateforme;
+     }
+    }
     
     ?>
 </body>
